1c28cc15e72872ba6d4562bbe97440e6c83e2500
zawmoewinv2ray <125839707+zawmoewin1@users.noreply.github.com>
2024-04-13 02:48:41 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '646C143F0C8F2E4F205F87B9EC3FCFFC';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
